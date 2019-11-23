651283fd500466701c4b5b4118269f2e34d16058
Nhat Dang <dangquangnhat18101994@gmail.com>
2019-11-23 16:57:23 +0700

    Init search images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { debounce } from 'lodash';
+import styles from './App.module.scss';
+
+const fetchImages = (searchQuery, setGallery) => async () => {
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=E4ChLnZqoVvB0fZXoi3DJjJqSQE2dp07&limit=8&q=${searchQuery}`);
+  if (response.status === 200) {
+    const result = await response.json();
+    setGallery(result.data);
+  }
+};
+
+const onSearch = (setSearchQuery) => (event) => {
+  const value = event.target.value;
+  setSearchQuery(value);
+}
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [gallery, setGallery] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.page}>
+      <input type="text" value={searchQuery} onChange={onSearch(setSearchQuery)} />
+      <button onClick={fetchImages(searchQuery, setGallery)}>click</button>
+      <div className={styles.gallery}>
+        {gallery.map((image) => (
+          <div key={image.id} className={styles.galleryItem}>
+            <img className={styles.galleryImage} src={image.images.original_still.url} alt={image.title} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

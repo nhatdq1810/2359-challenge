0cfd1e7d789778b89b4db57b4e5c948735cc1dc3
Nhat Dang <dangquangnhat18101994@gmail.com>
2019-11-24 16:06:57 +0700

    Style empty state in Favourite page

--- src/app/Favourites.js
+++ src/app/Favourites.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Gallery from '../components/Gallery';
+import styles from './Favourites.module.scss';
 
 export default function Favourites({ favouriteImages, likeImage }) {
   return (
     <>
-      {favouriteImages.length === 0 && (<div>There is nothing here!</div>)}
+      {favouriteImages.length === 0 && (<div className={styles.emptyState}>There is nothing here!</div>)}
       <Gallery gallery={favouriteImages} favouriteImages={favouriteImages} likeImage={likeImage} />
     </>
   )
